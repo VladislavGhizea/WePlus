46bc1b76b643802d15b5b978665fa9fffbcc3886
Vladislav <vladislavghizea666@gmail.com>
2025-01-14 17:51:42 +0100

    login frontend

---
+++ frontend/app/components/home/buttons/ThirdPartButton.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FaApple } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+
+interface Props {
+  text: string;
+}
+
+const ThirdPartButton: React.FC<Props> = ({ text }) => {
+  const color = text === "Apple" ? "bg-black text-white" : "bg-white";
+  return (
+    <button
+      className={`w-[23rem] h-[3rem] rounded-full flex items-center ${color}`}
+    >
+      {text === "Apple" ? (
+        <FaApple className="w-[2rem] h-[2rem] mx-2" color="white" />
+      ) : (
+        <FcGoogle className="w-[2rem] h-[2rem] mx-2" />
+      )}
+      <p className="text-base">continue with {text}</p>
+    </button>
+  );
+};
+
+export default ThirdPartButton;
---
+++ frontend/app/components/home/buttons/index.ts
@@ -0,0 +1,4 @@
+import TextInput from "./TextInput";
+import ActionButton from "./ActionButton";
+import ThirdPartButton from "./ThirdPartButton";
+export { TextInput, ActionButton, ThirdPartButton };
