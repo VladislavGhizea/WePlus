e7b7790ef9bb6213a0d881f206a88976af91f337
Christian <5aicondello.christian@peano.it>
2025-01-17 09:44:28 +0100

    versione con tutte le entità e repository, tolti tag repository

--- app/src/main/java/com/weplus/app/repository/AmbitoRepository.java
+++ app/src/main/java/com/weplus/app/repository/AmbitoRepository.java
@@ -1,12 +1,8 @@
 package com.weplus.app.repository;
 
 import com.weplus.app.entita.Ambito;
-import com.weplus.app.entita.PersonaFisica;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-@Repository
 public interface AmbitoRepository extends JpaRepository<Ambito, Integer> {
     // aggiungere query personalizzate se necessario
 
--- app/src/main/java/com/weplus/app/repository/PersonaFisicaRepository.java
+++ app/src/main/java/com/weplus/app/repository/PersonaFisicaRepository.java
@@ -2,10 +2,7 @@ package com.weplus.app.repository;
 
 import com.weplus.app.entita.PersonaFisica;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-@Repository
 public interface PersonaFisicaRepository extends JpaRepository<PersonaFisica, Integer> {
     // aggiungere query personalizzate se necessario
 
