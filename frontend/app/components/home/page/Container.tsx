966eb7239f6c4446da30e62b5ba6e43a0cb42313
Lorenzo <cosentino.lorenzo@peano.it>
2025-01-21 17:54:00 +0100

    Container aggiornato (non funziona) (si è rotto tutto) :)

--- frontend/app/components/home/page/Container.tsx
+++ frontend/app/components/home/page/Container.tsx
@@ -10,14 +10,28 @@ interface Props {
     city: string;
     isStarred: boolean;
     hobby: string[];
-    contact: { email: string; phone: number };
+    contact: { email: string, phone: number };
   };
 }
 
 const Container: React.FC<Props> = ({
   values: { name, surname, age, job, gender, city, isStarred, hobby, contact },
 }) => {
-  return <div className="bg-"></div>;
+  name="Mr";
+  surname="Griddy"
+  age=20
+  job="metodoindiano"
+  gender="Non Binario"
+  city="Savona"
+  isStarred=true
+  hobby=["Mangio sassi mentre guardo Minecraft"]
+  contact= {email: "asNan@gmail.com", phone: 94813874592}
+  
+  return <div className="bg-buttonGrey h-[13.5rem] w-[52rem]">
+    <div className="grid gridbg-white h-[10rem] w-[34rem]">
+      {/* <DataSection></DataSection> */}
+    </div>
+  </div>
 };
 
 export default Container;
--- frontend/app/components/home/page/index.ts
+++ frontend/app/components/home/page/index.ts
@@ -3,4 +3,5 @@ import Filters from "./Filters";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Category from "./Category";
-export { Container, Filters, Header, Sidebar, Category };
+import DataSection from "./DataSection"
+export { Container, Filters, Header, Sidebar, Category, DataSection };
