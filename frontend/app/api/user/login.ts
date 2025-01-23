a3bca8bb1313f8adb6f700130a741c5e0e125953
Lorenzo <cosentino.lorenzo@peano.it>
2025-01-23 10:45:54 +0100

    metodo GET

--- frontend/app/api/user/login.ts
+++ frontend/app/api/user/login.ts
@@ -10,21 +10,33 @@ interface User {
 }
 
 const login = async (username: string, password: string): Promise<boolean> => {
-    const response = await fetch(`${SERVER}/utentiGenerali`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-    const data: User[] = await response.json()
-    for (const user of data) {
-        if (user.username === username && user.password === password) {
-            localStorage.setItem('username', username)
-            localStorage.setItem('password', password)
-            localStorage.setItem('id', user.soggetto_id.toString())
-            localStorage.setItem('role', user.tipo)
-            return true; //accesso consentito
-        }
+    if(!username || !password){
+        console.error('Username e password sono richiesti');
+        return false;
     }
+    
+    try{
+        const response = await fetch(`${SERVER}/utentiGenerali`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            
+        })
+        const data: User[] = await response.json()
+        for (const user of data) {
+            if (user.username === username && user.password === password) {
+                localStorage.setItem('username', username)
+                localStorage.setItem('password', password)
+                localStorage.setItem('id', user.soggetto_id.toString())
+                localStorage.setItem('role', user.tipo)
+                return true; //accesso consentito
+            }
+        }
+
+    }catch(error){
+        console.error(`Abbiamo avuto un problema con il login: ${error}`)
+    }
+    
     return false; //accesso negato
 }
